6c69aa92b3f755b38cfabff0474d9ba7abdc06a5
CAT <catdematos98@gmail.com>
2019-11-09 14:46:11 -0500

    building a chain

--- app/src/main/java/com/example/securevotes/MainActivity.java
+++ app/src/main/java/com/example/securevotes/MainActivity.java
@@ -5,21 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "ChainTesting";
+    public static ArrayList<VoteBlock> voteChain = new ArrayList<VoteBlock>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
 
-        VoteBlock genesisBlock = new VoteBlock("Chris", "0");
-        VoteBlock vote2 = new VoteBlock("Chris", genesisBlock.hash);
-        VoteBlock vote3 = new VoteBlock("Chris", vote2.hash);
+        voteChain.add(new VoteBlock("Chris", "0"));
+        voteChain.add(new VoteBlock("Chris", voteChain.get(voteChain.size()-1).hash));
+        voteChain.add(new VoteBlock("Chris", voteChain.get(voteChain.size()-1).hash));
 
-        System.out.println(genesisBlock.hash);
-        Log.i(TAG, genesisBlock.hash);
-        Log.i(TAG, vote2.hash);
-        Log.i(TAG, vote3.hash);
+        //Todo - Gson library
+        for(VoteBlock vote : voteChain){
+            Log.i(TAG, vote.hash);
+        }
     }
 }
